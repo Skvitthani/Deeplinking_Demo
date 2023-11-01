90182755373666b9a8b8bd2ef2eee96225df55bc
Sanket V <sanketvithani6@gmail.com>
2023-11-01 18:14:39 +0530

    => Integrate Deeplinking

--- src/navigations/StckNavigate.js
+++ src/navigations/StckNavigate.js
@@ -1,15 +1,31 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {NavigationContainer} from '@react-navigation/native';
+import React, {useEffect} from 'react';
+import {StyleSheet} from 'react-native';
 import HomeScreen from '../screens/HomeScreen';
 import ProductDetails from '../screens/ProductDetails';
+import dynamicLinks from '@react-native-firebase/dynamic-links';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {NavigationContainer, useNavigation} from '@react-navigation/native';
 
 const Stack = createNativeStackNavigator();
 
 const StckNavigate = () => {
+  const HandelDeepLinking = () => {
+    const navigation = useNavigation();
+    const handelLink = async link => {
+      let productId = link?.url?.split('?').pop();
+      navigation.navigate('Product', {productId: productId});
+    };
+    useEffect(() => {
+      const unsubscribe = dynamicLinks().onLink(handelLink);
+      return () => unsubscribe();
+    }, []);
+
+    return null;
+  };
+
   return (
     <NavigationContainer>
+      <HandelDeepLinking />
       <Stack.Navigator screenOptions={{headerShown: false}}>
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Product" component={ProductDetails} />
