83c4b0fc5ebdb52acb52a1ce139bcdc985fb8999
Sanket V <sanketvithani6@gmail.com>
2023-12-04 16:32:00 +0530

    => Add images and working on reAnimated

---
+++ src/helper/DummyImage.js
@@ -0,0 +1,170 @@
+export const IMAGE_CATEGORIES = [
+  {
+    id: 0,
+    name: 'Nature',
+    url: 'https://images.pexels.com/photos/13979460/pexels-photo-13979460.jpeg',
+    previewUrl:
+      'https://images.pexels.com/photos/13979460/pexels-photo-13979460.jpeg?auto=compress&cs=tinysrgb&h=130',
+  },
+  {
+    id: 1,
+    name: 'Travel',
+    url: 'https://images.pexels.com/photos/3889742/pexels-photo-3889742.jpeg',
+    previewUrl:
+      'https://images.pexels.com/photos/3889742/pexels-photo-3889742.jpeg?auto=compress&cs=tinysrgb&h=130',
+  },
+  {
+    id: 2,
+    name: 'Technology',
+    url: 'https://images.pexels.com/photos/2047905/pexels-photo-2047905.jpeg',
+    previewUrl:
+      'https://images.pexels.com/photos/2047905/pexels-photo-2047905.jpeg?auto=compress&cs=tinysrgb&h=130',
+  },
+  {
+    id: 3,
+    name: 'Food',
+    url: 'https://images.pexels.com/photos/1199957/pexels-photo-1199957.jpeg',
+    previewUrl:
+      'https://images.pexels.com/photos/1199957/pexels-photo-1199957.jpeg?auto=compress&cs=tinysrgb&h=130',
+  },
+  {
+    id: 4,
+    name: 'Music',
+    url: 'https://images.pexels.com/photos/2909367/pexels-photo-2909367.jpeg',
+    previewUrl:
+      'https://images.pexels.com/photos/2909367/pexels-photo-2909367.jpeg?auto=compress&cs=tinysrgb&h=130',
+  },
+  {
+    id: 5,
+    name: 'Sports',
+    url: 'https://images.pexels.com/photos/1837687/pexels-photo-1837687.jpeg',
+    previewUrl:
+      'https://images.pexels.com/photos/1837687/pexels-photo-1837687.jpeg?auto=compress&cs=tinysrgb&h=130',
+  },
+  {
+    id: 6,
+    name: 'Art and Design',
+    url: 'https://images.pexels.com/photos/6925162/pexels-photo-6925162.jpeg',
+    previewUrl:
+      'https://images.pexels.com/photos/6925162/pexels-photo-6925162.jpeg?auto=compress&cs=tinysrgb&h=130',
+  },
+  {
+    id: 7,
+    name: 'Motivational',
+    url: 'https://images.pexels.com/photos/3806690/pexels-photo-3806690.jpeg',
+    previewUrl:
+      'https://images.pexels.com/photos/3806690/pexels-photo-3806690.jpeg?auto=compress&cs=tinysrgb&h=130',
+  },
+  {
+    id: 8,
+    name: 'Animals',
+    url: 'https://images.pexels.com/photos/2313396/pexels-photo-2313396.jpeg',
+    previewUrl:
+      'https://images.pexels.com/photos/2313396/pexels-photo-2313396.jpeg?auto=compress&cs=tinysrgb&h=130',
+  },
+  {
+    id: 9,
+    name: 'Fashion',
+    url: 'https://images.pexels.com/photos/2002717/pexels-photo-2002717.jpeg',
+    previewUrl:
+      'https://images.pexels.com/photos/2002717/pexels-photo-2002717.jpeg?auto=compress&cs=tinysrgb&h=130',
+  },
+  {
+    id: 10,
+    name: 'Health',
+    url: 'https://images.pexels.com/photos/4325476/pexels-photo-4325476.jpeg',
+    previewUrl:
+      'https://images.pexels.com/photos/4325476/pexels-photo-4325476.jpeg?auto=compress&cs=tinysrgb&h=130',
+  },
+  {
+    id: 11,
+    name: 'Buildings',
+    url: 'https://images.pexels.com/photos/3224533/pexels-photo-3224533.jpeg',
+    previewUrl:
+      'https://images.pexels.com/photos/3224533/pexels-photo-3224533.jpeg?auto=compress&cs=tinysrgb&h=130',
+  },
+  {
+    id: 12,
+    name: 'Business',
+    url: 'https://images.pexels.com/photos/1684151/pexels-photo-1684151.jpeg',
+    previewUrl:
+      'https://images.pexels.com/photos/1684151/pexels-photo-1684151.jpeg?auto=compress&cs=tinysrgb&h=130',
+  },
+  {
+    id: 13,
+    name: 'Vintage',
+    url: 'https://images.pexels.com/photos/3182452/pexels-photo-3182452.jpeg',
+    previewUrl:
+      'https://images.pexels.com/photos/3182452/pexels-photo-3182452.jpeg?auto=compress&cs=tinysrgb&h=130',
+  },
+  {
+    id: 14,
+    name: 'Love',
+    url: 'https://images.pexels.com/photos/14106982/pexels-photo-14106982.jpeg',
+    previewUrl:
+      'https://images.pexels.com/photos/14106982/pexels-photo-14106982.jpeg?auto=compress&cs=tinysrgb&h=130',
+  },
+  {
+    id: 15,
+    name: 'Summer',
+    url: 'https://images.pexels.com/photos/4784570/pexels-photo-4784570.jpeg',
+    previewUrl:
+      'https://images.pexels.com/photos/4784570/pexels-photo-4784570.jpeg?auto=compress&cs=tinysrgb&h=130',
+  },
+  {
+    id: 16,
+    name: 'Holidays',
+    url: 'https://images.pexels.com/photos/5344847/pexels-photo-5344847.jpeg',
+    previewUrl:
+      'https://images.pexels.com/photos/5344847/pexels-photo-5344847.jpeg?auto=compress&cs=tinysrgb&h=130',
+  },
+  {
+    id: 17,
+    name: 'Transportation',
+    url: 'https://images.pexels.com/photos/2280165/pexels-photo-2280165.jpeg',
+    previewUrl:
+      'https://images.pexels.com/photos/2280165/pexels-photo-2280165.jpeg?auto=compress&cs=tinysrgb&h=130',
+  },
+  {
+    id: 18,
+    name: 'Backgrounds',
+    url: 'https://images.pexels.com/photos/207353/pexels-photo-207353.jpeg',
+    previewUrl:
+      'https://images.pexels.com/photos/207353/pexels-photo-207353.jpeg?auto=compress&cs=tinysrgb&h=130',
+  },
+  {
+    id: 19,
+    name: 'Sunset',
+    url: 'https://images.pexels.com/photos/2893556/pexels-photo-2893556.jpeg',
+    previewUrl:
+      'https://images.pexels.com/photos/2893556/pexels-photo-2893556.jpeg?auto=compress&cs=tinysrgb&h=130',
+  },
+  {
+    id: 20,
+    name: 'Education',
+    url: 'https://images.pexels.com/photos/3184644/pexels-photo-3184644.jpeg',
+    previewUrl:
+      'https://images.pexels.com/photos/3184644/pexels-photo-3184644.jpeg?auto=compress&cs=tinysrgb&h=130',
+  },
+  {
+    id: 21,
+    name: 'Car',
+    url: 'https://images.pexels.com/photos/5086489/pexels-photo-5086489.jpeg',
+    previewUrl:
+      'https://images.pexels.com/photos/5086489/pexels-photo-5086489.jpeg?auto=compress&cs=tinysrgb&h=130',
+  },
+  {
+    id: 22,
+    name: 'Sky',
+    url: 'https://images.pexels.com/photos/4601807/pexels-photo-4601807.jpeg',
+    previewUrl:
+      'https://images.pexels.com/photos/4601807/pexels-photo-4601807.jpeg?auto=compress&cs=tinysrgb&h=130',
+  },
+  {
+    id: 23,
+    name: 'Beach',
+    url: 'https://images.pexels.com/photos/1032650/pexels-photo-1032650.jpeg',
+    previewUrl:
+      'https://images.pexels.com/photos/1032650/pexels-photo-1032650.jpeg?auto=compress&cs=tinysrgb&h=130',
+  },
+];
