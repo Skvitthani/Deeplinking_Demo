0a5d08b5301b30b349b3e1be3753d2994f92a424
Sanket V <sanketvithani6@gmail.com>
2023-12-18 10:05:17 +0530

    => Implement reeact-native-push-notification

--- index.js
+++ index.js
@@ -5,5 +5,20 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import PushNotification from 'react-native-push-notification';
+
+PushNotification.configure({
+  onRegister: token => {
+    console.log('Token', token);
+  },
+  onNotification: notification => console.log('NOTIFICATION', notification),
+  permissions: {
+    alert: true,
+    badge: true,
+    sound: true,
+  },
+  popInitialNotification: true,
+  requestPermissions: true,
+});
 
 AppRegistry.registerComponent(appName, () => App);
